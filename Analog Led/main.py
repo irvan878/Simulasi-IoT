74611aff06b67eb59639bba66f5d4172ed622d55
irvan878 <irfannur878@gmail.com>
2025-05-15 21:08:56 +0700

    Update main.py

--- Analog Led/main.py
+++ Analog Led/main.py
@@ -1,1 +1,14 @@
+from gpio import * 
+from time import *  
+def main():
+  pinMode(0, IN)
+  pinMode(1, OUT)
+  print("light on")
+  while True: 
+    if digitalRead(0)== HIGH:
+      digitalWrite(1, HIGH)
+    else : 
+      digitalWrite(1, LOW)
 
+if __name__ == "__main__": 
+  main()
