d1c0ba2fa6d3ede149f6e2f62e37684a96ecb3bc
irvan878 <irfannur878@gmail.com>
2025-05-15 21:13:54 +0700

    Create main.py

---
+++ Smart Home Digital/main.py
@@ -0,0 +1,21 @@
+from gpio import *
+from time import *
+
+def sensorGerakan():
+	nilai = digitalRead(0)
+	if nilai == 0:
+		print("Tidak ada gerakan")
+		customWrite(1, '0')
+		customWrite(2, '0')
+	else:
+		print("Ada Gerakan")
+		customWrite(1, '1')
+		customWrite(2, '1')
+def main():
+	add_event_detect(0, sensorGerakan)
+	
+	while True:
+		delay(1000)
+		
+if __name__ == "__main__":
+	main()
